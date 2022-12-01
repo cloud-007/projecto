204b106f315a8b4f3292c0ce7e9dee92cb25c275
Mazharul Islam <mazharulislam742@yahoo.com>
2022-12-01 17:26:59 +0600

    added environ file

--- projecto/settings.py
+++ projecto/settings.py
@@ -12,8 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env((BASE_DIR / '.env').as_posix())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -134,7 +139,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = 'homehunt.bd@gmail.com'
-EMAIL_HOST_PASSWORD = 'tzmdhchazyiatluk'
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
