0fe794b4ce11162982f0f781af7e05b48c28a65d
Mazharul Islam <mazharulislam742@yahoo.com>
2023-02-12 23:35:20 +0600

    updated create new course html file , added custom tag

--- project_management/templatetags/custom_tags.py
+++ project_management/templatetags/custom_tags.py
@@ -1,8 +1,6 @@
-import datetime
-
 from django import template
 
-from project_management.models import Course, Proposal
+from project_management.models import Course, Proposal, CourseState
 
 register = template.Library()
 
@@ -23,17 +21,16 @@ def proposal_4801_count(queryset, initials):  # Only one argument.
 
 
 def get_course_count(course_code, initials):
-    running_courses = Course.objects.filter(
-        deadline__range=(
-            datetime.datetime.now().date(),
-            datetime.date(2500, 1, 1))).order_by(
-        'deadline'
+    running_courses = Course.objects.filter(state=CourseState.RUNNING, course_code=course_code).order_by(
+        'start_time'
     )
-    running_courses = running_courses.filter(course_id=course_code)
-    print("Running Courses")
-    print(running_courses)
     proposals = Proposal.objects.filter(course__in=running_courses, assigned_supervisor__initials=initials)
     cnt = 0
     for proposal in proposals:
         cnt = cnt + proposal.students.count()
     return cnt
+
+
+@register.filter(name='first_word')
+def first_word(sentence):  # Only one argument.
+    return sentence.split(' ')[0]
