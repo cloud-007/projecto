1229723fae56c6da09a7025e0c4e3c34e90037d8
Mazharul Islam <mie.mazharul@gmail.com>
2023-04-17 02:27:31 +0600

    Revert "Update setting and requirements file"
    This reverts commit 36674e981af58d591ee6e02da3da3cda221cf096.

--- projecto/settings.py
+++ projecto/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,7 +84,10 @@ WSGI_APPLICATION = 'projecto.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
